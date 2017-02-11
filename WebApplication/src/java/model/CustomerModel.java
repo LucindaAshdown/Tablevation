f1579ff4e561d18865a77e8e26e5f8eb1c3e7fc8
bharris2657 <up735437@myport.ac.uk>
2017-02-11 13:40:38 +0000

    Insert and update customer model methods complete

--- WebApplication/src/java/model/CustomerModel.java
+++ WebApplication/src/java/model/CustomerModel.java
@@ -41,14 +41,35 @@ public class CustomerModel implements Model{
     
     @Override
     public void insert() {
-        PreparedStatement ps;
         String query = "INSERT INTO Customer VALUES (?,?,?,?,?)";
+        try{
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, this.email);
+            ps.setString(2, this.password);
+            ps.setString(3, this.forename);
+            ps.setString(4, this.surname);
+            ps.setString(5, this.contactNumber);
+            
+            ps.executeUpdate();
+        }catch(SQLException ex){
+            ex.printStackTrace();
+        }
     }
 
     @Override
     public void update() {
-        PreparedStatement ps;
-        String query = "UPDATE Customer SET (?,?,?)";
+        String query = "UPDATE Customer SET (?,?,?) WHERE Customer.email = ?";
+        try{
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, this.password);
+            ps.setString(2, this.forename);
+            ps.setString(3, this.surname);
+            ps.setString(4, this.email);
+            
+            ps.executeUpdate();
+        }catch(SQLException ex){
+            ex.printStackTrace();
+        }
     }
 
     @Override
